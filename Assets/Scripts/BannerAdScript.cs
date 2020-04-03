5e85c77636e675fb24f902c528ec1a9e68b1c329
Luiz Henrique Moreira de Souza <luizhm0reira@gmail.com>
2020-04-03 00:22:49 -0700

    04-03-2020 2

---
+++ Assets/Scripts/BannerAdScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class BannerAdScript : MonoBehaviour
+{
+
+    public string gameId = "3488728";
+    public string placementId = "bannerPlacement";
+    public bool testMode = true;
+    //public BannerPosition bannerPosition = BannerPosition.BOTTOM_CENTER;
+
+    void Start()
+    {
+        Advertisement.Initialize(gameId, testMode);
+        StartCoroutine(ShowBannerWhenReady());
+    }
+
+    IEnumerator ShowBannerWhenReady()
+    {
+        while (!Advertisement.IsReady(placementId))
+        {
+            yield return new WaitForSeconds(0.5f);
+        }        
+        //Advertisement.Banner.SetPosition(bannerPosition);
+        //Advertisement.Banner.Show(placementId);
+    }
+}
